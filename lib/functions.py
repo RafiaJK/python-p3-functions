e9896a31e03f0d645fcbe069182f5b7ab32509d7
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-04-03 16:04:09 -0400

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Rafia")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1+num2
 
 def halve(number):
-    pass
+    return number/2
